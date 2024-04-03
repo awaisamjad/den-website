fd9ef5dd3e07133fd22926d7952efe4c62f80430
Awais Amjad <amjadawais08@gmail.com>
2024-04-03 17:48:21 +0100

    created the cards with tailwind

--- src/lib/components/MemoryCard.svelte
+++ src/lib/components/MemoryCard.svelte
@@ -2,78 +2,20 @@
 	export let gif = '';
 	export let title = '';
 	export let content = '';
-
-	function toggleDropDown() {
-		const dropdownContent = document.getElementById('dropdown-content');
-		dropdownContent.classList.toggle('show');
-		const dropdownButton = document.querySelector('.dropdown');
-		if (dropdownContent.classList.contains('show')) {
-			dropdownButton.textContent = 'Hide';
-		} else {
-			dropdownButton.textContent = 'Show More';
-		}
-	}
+	let showContent = false;
 </script>
 
-<div class="card">
-	<div class="icon-container">
-		<div class="icon">
-			<img src={gif} alt="GIF" class="icon" />
+<div class="flex flex-col items-center w-40 bg-red-300 mx-3 my-3 rounded-lg">
+	<div class="w-36 h-36 rounded-full flex items-center mt-2">
+		<img src={gif} alt="GIF" class="w-36 h-36 rounded-full" />
+	</div>
+	<span class="mt-4 text-lg font-semibold text-center">{title}</span>
+	<button class="my-2 bg-red-600 rounded-lg w-32 h-8" on:click={() => (showContent = !showContent)}
+		>Show More</button
+	>
+	{#if showContent}
+		<div class="mt-2 p-3 text-base font-serif">
+			{content}
 		</div>
-	</div>
-	<span class="title">{title}</span>
-	<button class="dropdown">Show More</button>
-	<div class="content" id="dropdown-content">
-		{content}
-	</div>
+	{/if}
 </div>
-
-<style>
-	.card {
-		display: flex;
-		width: 150px;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		padding: 24px;
-		margin-top: 10px;
-		margin-right: 5px;
-		background-color: rgba(207, 126, 126, 0.384);
-		border-radius: 15px;
-		box-shadow:
-			0px 10px 15px -3px rgba(0, 0, 0, 0.1),
-			0px 4px 6px -2px rgba(0, 0, 0, 0.05);
-	}
-	.icon-container {
-		display: flex;
-		align-items: center;
-		margin-top: 10px;
-		outline: red 5px solid;
-	}
-	.icon {
-		width: 144px;
-		height: 144px;
-		background-color: #e2e8f0;
-		border-radius: 50%;
-		align-items: center;
-	}
-
-	.title {
-		margin-top: 16px;
-		font-size: 20px;
-		font-weight: 600;
-		align-items: center;
-	}
-
-	.bottom-section {
-		margin-top: 16px;
-	}
-
-	/* .content {
-		display: none;
-	} */
-
-	.show {
-		display: block;
-	}
-</style>
