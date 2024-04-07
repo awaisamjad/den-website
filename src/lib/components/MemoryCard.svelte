4542cd4964a6c06d45db9fb5f865088d9416dad2
Awais Amjad <amjadawais08@gmail.com>
2024-04-07 18:03:19 +0100

    stuff

--- src/lib/components/MemoryCard.svelte
+++ src/lib/components/MemoryCard.svelte
@@ -8,22 +8,28 @@
     }
 </script>
 
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
 <div id="card" class="w-card h-card perspective" on:click={flipCard}>
     <div id="card-inner" class:flipped={isFlipped}>
-        <div id="card-front" class="card-face flex flex-col items-center bg-blue-900 mx-3 mt-3 rounded-lg shadow-card">
+		<!-- FRONT CARD -->
+        <div id="card-front" class="card-face flex flex-col items-center bg-blue-900 mx-3 mt-3 rounded-lg">
             <div class = "mt-4" >
 				<span class="text-2xl font-semibold text-center text-white">{title}</span>
 			</div>
 			<img id="gif" src={gif} alt="GIF" class="w-logo h-logo rounded-lg mt-3" />
         </div>
-        <div id="card-back" class="card-face flex flex-col items-center bg-blue-900 mx-3 mt-3 rounded-lg shadow-card">
-            <div class="flex flex-col items-center w-card h-card bg-blue-900 mx-3 mt-3 rounded-lg shadow-card">
+
+		<!-- BACK CARD -->
+        <div id="card-back" class="card-face flex flex-col items-center bg-blue-900 mx-3 my-3 rounded-lg">
+            <div class="flex flex-col items-center w-card h-card bg-blue-900 mx-3 mt-3 rounded-sm">
 				<div class = "mt-4" >
 					<span class="text-2xl font-semibold text-center text-white">{title}</span>
 				</div>
 				<p class="text-white text-center mt-3">{content}</p>
 			</div>
         </div>
+
     </div>
 </div>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import MemoryCard from '../lib/components/MemoryCard.svelte';
 	import FlipCard from '../lib/components/FlipCard.svelte';
-    import Test from '../lib/components/Test.svelte';
+	import Test from '../lib/components/Test.svelte';
 	let data = [
 		{
 			title: 'Aortic Dissection',
@@ -56,8 +56,21 @@
 	];
 </script>
 
-<div id="main" class="bg-blue-200 min-h-screen">
+<div id="main" class="bg-blue-200 h-screen">
+	<!-- TITLE and LOGO -->
+	<div class="flex justify-evenly">
+		<h1 class="title-text-style ">Memory</h1>
+		<img
+			src="https://media.giphy.com/media/3o7TKz9bX9v9KzCnXq/giphy.gif"
+			alt="GIF"
+			class="w-24 h-24 rounded-lg mt-4"
+		/>
+		<h1 class="title-text-style">Project</h1>
+	</div>
+
+	<!-- Display of cards -->
 	<ul class="list">
+        <div class="flex flex-wrap justify-center items-center">
 		{#each data as item}
 			<MemoryCard
 				gif="https://media.giphy.com/media/3o7TKz9bX9v9KzCnXq/giphy.gif"
@@ -66,9 +79,10 @@
 				content={item.content}
 			/>
 		{/each}
+    </div>
 	</ul>
+    
 </div>
-
 
 <style>
 	.list {
