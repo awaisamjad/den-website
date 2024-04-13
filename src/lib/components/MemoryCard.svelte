fcf17ba0d87ebe44680a035dcba99bdb57c099ef
Awais Amjad <amjadawais08@gmail.com>
2024-04-13 16:26:21 +0100

    added urls to the cards

--- src/lib/components/MemoryCard.svelte
+++ src/lib/components/MemoryCard.svelte
@@ -1,63 +1,71 @@
 <script>
-    export let gif = '';
+	export let gif = '';
 	export let title = '';
 	export let content = '';
-    let isFlipped = false;
-    function flipCard() {
-        isFlipped = !isFlipped;
-    }
+	export let url = '';
+	let isFlipped = false;
+	function flipCard() {
+		isFlipped = !isFlipped;
+	}
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div id="card" class="w-card h-card perspective mb-3" on:click={flipCard}>
-    <div id="card-inner" class:flipped={isFlipped}>
+	<div id="card-inner" class:flipped={isFlipped}>
 		<!-- FRONT CARD -->
-        <div id="card-front" class="card-face flex flex-col items-center bg-blue-900 mx-3 mt-3 rounded-lg">
-            <div class = "mt-4" >
+		<div
+			id="card-front"
+			class="card-face flex flex-col items-center bg-blue-900 mx-3 mt-3 rounded-lg"
+		>
+			<div class="mt-4">
 				<span class="text-2xl font-semibold text-center text-white">{title}</span>
 			</div>
 			<img id="gif" src={gif} alt="GIF" class="w-logo h-logo rounded-lg mt-3" />
-        </div>
+		</div>
 
 		<!-- BACK CARD -->
-        <div id="card-back" class="card-face flex flex-col items-center bg-blue-900 mx-3 my-3 rounded-lg">
-            <div class="flex flex-col items-center w-card h-card bg-blue-900 mx-3 mt-3 rounded-sm">
-				<div class = "mt-4" >
+		<div
+			id="card-back"
+			class="card-face flex flex-col items-center bg-blue-900 mx-3 my-3 rounded-lg"
+		>
+			<div class="flex flex-col items-center w-card h-card bg-blue-900 mx-3 mt-3 rounded-sm">
+				<div class="mt-4">
 					<span class="text-2xl font-semibold text-center text-white">{title}</span>
 				</div>
-                <div class="mt-3">
-                    <p class="text-white text-left m-5 text-2xl">{content}</p>
-                </div>
+				<div class="mt-3">
+					<p class="text-white text-center m-5 text-2xl">{content}</p>
+				</div>
 			</div>
-            <button class="w-24 h-10 mb-8 bg-blue-500 text-white rounded-lg">Open</button>
-        </div>
-
-    </div>
+			<a href={url} target="_blank">
+				<button class="w-24 h-10 mb-8 bg-blue-500 text-white rounded-lg text-lg"> Open </button>
+			</a>
+		</div>
+	</div>
 </div>
 
 <style>
-    .perspective {
-        perspective: 1000px;
-    }
-    #card-inner {
-        position: relative;
-        width: 100%;
-        height: 100%;
-        text-align: center;
-        transition: transform 0.8s;
-        transform-style: preserve-3d;
-    }
-    .card-face {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        backface-visibility: hidden;
-    }
-    #card-back {
-        transform: rotateY(180deg);
-    }
-    .flipped {
-        transform: rotateY(180deg);
-    }
+	.perspective {
+		perspective: 1000px;
+	}
+	#card-inner {
+		position: relative;
+		width: 100%;
+		height: 100%;
+		text-align: center;
+		transition: transform 0.8s;
+		transform-style: preserve-3d;
+	}
+	.card-face {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		backface-visibility: hidden;
+	}
+	#card-back {
+		transform: rotateY(180deg);
+	}
+	.flipped {
+		transform: rotateY(180deg);
+	}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,20 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {
       height: {
         'card': '600px',
-        'logo' : '520px',
+        'logo': '520px',
       },
       width: {
         'card': '400px',
-        'logo' : '380px',
+        'logo': '380px',
       },
       boxShadow: {
         'card': ' 8px 9px rgba(204, 204, 204, 0.5)',
